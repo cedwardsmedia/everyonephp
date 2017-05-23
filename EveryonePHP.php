2ca61418675e860c90c120649201041ca2931667
Corey Edwards <cedwardsmedia@gmail.com>
2017-05-23 01:49:38 -0500

    Minor updates
    - Added CURLE_COULDNT_RESOLVE_HOST error response
    - Added CURLE_COULDNT_RESOLVE_PROXY error response
    - Added license to EveryonePHP.php

--- EveryonePHP.php
+++ EveryonePHP.php
@@ -1,4 +1,20 @@
 <?php
+
+/*
+ *  Copyright (c) 2015-2017, Corey Edwards
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+ *
+ *  1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+ *
+ *  2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+ *
+ *  3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
 class EveryonePHP {
 
     public $sid; // EveryoneAPI account SID
@@ -41,6 +57,10 @@ class EveryonePHP {
                 $this->error = "HTTP 500 Internal Server Error: EveryoneAPI is currently experiencing technical difficulties.";
             } elseif (strstr($exception->getMessage(), '503')) {
                 $this->error = "HTTP 503 Service Unavailable: EveryoneAPI is currently experiencing service outages.";
+            } elseif (strstr($exception->getMessage(), 'cURL error 6')) {
+		$this->error = "cURL error 6 - Could not resolve host api.everyoneapi.com";
+            } elseif (strstr($exception->getMessage(), 'cURL error 5')) {
+		$this->error = "cURL error 5 - Could not resolve proxy";
             } else {
                 $this->error = "An error as occurred. GuzzleHttp said: " . $exception->getMessage();
             }
